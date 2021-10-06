d1c8fa19c05a58a4db55d41f517d8f18024fb3e8
Conor Rankine <conor.rankine@ncl.ac.uk>
2021-10-06 10:53:22 +0100

    overhauled (W)ACSF code; OO automatic parameterisation following Marquetand's approach

--- xanesnet/descriptors.py
+++ xanesnet/descriptors.py
@@ -39,17 +39,15 @@ class VectorDescriptor(ABC):
     
     def __init__(
         self, 
-        r_min: float = 0.0, 
-        r_max: float = 6.0
+        r_min: float, 
+        r_max: float
     ):
         """
         Args:
             r_min (float): The minimum radial cutoff distance (in A) around
-                the absorption site; should be 0.0.
-                Defaults to 0.0.
+                the absorption site.
             r_max (float): The maximum radial cutoff distance (in A) around
                 the absorption site.
-                Defaults to 6.0.
         """
 
         if isinstance(r_min, (int, float)) and r_min >= 0.0:
@@ -63,10 +61,7 @@ class VectorDescriptor(ABC):
             raise ValueError(f'expected r_max: int/float > r_min; got {r_max}')        
 
     @abstractmethod
-    def transform(
-        self, 
-        system: Atoms
-    ) -> np.ndarray:
+    def transform(self, system: Atoms) -> np.ndarray:
         """
         Transforms a molecular system into a fingerprint feature vector, or
         'descriptor', that encodes the local environment around an absorption
@@ -103,7 +98,7 @@ class RDC(VectorDescriptor):
     def __init__(
         self, 
         r_min: float = 0.0,
-        r_max: float = 6.0,
+        r_max: float = 8.0,
         dr: float = 0.01,
         alpha: float = 10.0
     ):
@@ -138,10 +133,7 @@ class RDC(VectorDescriptor):
         nr_aux = int(np.absolute(self.r_max - self.r_min) / self.dr) + 1
         self.r_aux = np.linspace(self.r_min, self.r_max, nr_aux)
 
-    def transform(
-        self, 
-        system: Atoms
-    ) -> np.ndarray:
+    def transform(self, system: Atoms) -> np.ndarray:
         
         if not isinstance(system, Atoms):
             raise TypeError(f'systems passed as arguments to .transform ',
@@ -188,158 +180,320 @@ class WACSF(VectorDescriptor):
     def __init__(
         self,
         r_min: float = 0.0,
-        r_max: float = 6.0,
-        g2_vars: list = None,
-        g4_vars: list = None,
-        with_weighting = True
+        r_max: float = 8.0,
+        n_g2: int = 0,
+        n_g4: int = 0,
+        l: list = [1.0, -1.0],
+        z: list = [1.0],
+        g2_parameterisation: str = 'shifted',
+        g4_parameterisation: str = 'centred'
     ):
         """
         Args:
-            r_min (float): The minimum radial cutoff distance (in A) around
-                the absorption site; should be 0.0.
+            r_min (float, optional): The minimum radial cutoff distance (in A)
+                around the absorption site; should be 0.0.
                 Defaults to 0.0.
-            r_max (float): The maximum radial cutoff distance (in A) around
-                the absorption site.
-                Defaults to 6.0.
-            g2_vars (list, optional): A list of eta (h) and mu (m) pairs,
-                i.e. [[h1,m1],[h2,m2],...,[hn,mn]], used to construct G2
-                (radial) symmetry functions.
-                Defaults to None.
-            g4_vars (list, optional): A list of eta (h), mu (m), lambda (l),
-                and zeta (z) quintuples, i.e. [[h1,m1,l1,z1],[h2,m2,l2,z2],
-                ...,[hn,mn,ln,zn]], used to construct G4 (angular) symmetry
-                functions.
-                Defaults to None.
+            r_max (float, optional): The maximum radial cutoff distance (in A)
+                around the absorption site.
+                Defaults to 8.0.
+            n_g2 (int, optional): The number of G2 symmetry functions to use
+                for encoding.
+                Defaults to 0.
+            n_g4 (int, optional): The number of G4 symmetry functions to use
+                for encoding.
+                Defaults to 0.
+            l (list, optional): List of lambda values for G4 symmetry function
+                encoding. For details, see Marquetand et al.; J. Chem. Phys.,
+                2018, 148, 241709 (DOI: 10.1063/1.5019667).
+                Defaults to [1.0, -1.0].
+            z (list, optional): List of zeta values for G4 symmetry function
+                encoding. For details, see Marquetand et al.; J. Chem. Phys.,
+                2018, 148, 241709 (DOI: 10.1063/1.5019667).
+                Defaults to [1.0].
+            g2_parameterisation (str, optional): The strategy to use for G2
+                symmetry function parameterisation; choices are 'shifted' or
+                'centred'. For details, see Marquetand et al.; J. Chem. Phys.,
+                2018, 148, 241709 (DOI: 10.1063/1.5019667).
+                Defaults to 'shifted'.
+            g4_parameterisation (str, optional): The strategy to use for G4
+                symmetry function parameterisation; choices are 'shifted' or
+                'centred'. For details, see Marquetand et al.; J. Chem. Phys.,
+                2018, 148, 241709 (DOI: 10.1063/1.5019667).
+                Defaults to 'centred'.
         """
 
         super().__init__(r_min, r_max)
 
-        self.use_g2 = g2_vars is not None
-        if self.use_g2:
-            g2_vars_ = np.array(g2_vars, dtype = 'float32')
-            self.n_g2 = len(g2_vars_)
-            try:
-                self.g2_h, self.g2_m = g2_vars_.T
-            except ValueError:
-                raise ValueError(f'g2_vars is not formatted properly; ',
-                    'expected [[h1,m1],[h2,m2],...,[hn,mn]]')
-        else:
-            self.n_g2 = 0
+        self.n_g2 = n_g2
+        self.n_g4 = n_g4
+        if self.n_g4:
+            self.l = l
+            self.z = z
+        self.g2_parameterisation = g2_parameterisation
+        self.g4_parameterisation = g4_parameterisation
 
-        self.use_g4 = g4_vars is not None
-        if self.use_g4:
-            g4_vars_ = np.array(g4_vars, dtype = 'float32')
-            self.n_g4 = len(g4_vars_)
-            try:
-                self.g4_h, self.g4_m, self.g4_l, self.g4_z = g4_vars_.T
-            except ValueError:
-                raise ValueError(f'g4_vars is not formatted properly; ',
-                    'expected [[h1,m1,l1,z1],[h2,m2,l2,z2],...,[hn,mn,ln,zn]]')
-        else:
-            self.n_g4 = 0
+        if self.n_g2:
+            self.g2_transformer = G2SymmetryFunctionTransformer(
+                self.n_g2,
+                r_min = self.r_min,
+                r_max = self.r_max,
+                parameterisation = self.g2_parameterisation
+            )
 
-        if isinstance(with_weighting, bool):
-            self.with_weighting = with_weighting
-        else:
-            raise ValueError(f'expected with_weighting: bool (True/False); ',
-                'got {with_weighting}')
-
-    def transform(
-        self, 
-        system: Atoms
-    ) -> np.ndarray:
-
-        if not isinstance(system, Atoms):
-            raise TypeError(f'systems passed as arguments to .transform ',
-                'should be ase.Atoms objects')
+        if self.n_g4:
+            self.g4_transformer = G4SymmetryFunctionTransformer(
+                self.n_g4,
+                r_min = self.r_min,
+                r_max = self.r_max,
+                l = self.l,
+                z = self.z,
+                parameterisation = self.g4_parameterisation
+            )
+        
+    def transform(self, system: Atoms) -> np.ndarray:
 
         rij_in_range = system.get_distances(0, range(len(system))) < self.r_max
         system = system[rij_in_range]
 
-        ij = [[0,j] for j in range(1, len(system))]
-        if len(ij) < 1:
-            raise RuntimeError(f'too few atoms within {self.r_max:.2f} A of ',
-                'the absorption site to set up non-zero radial symmetry ',
-                'functions (no pairs)')
-        else:
-            ij = np.array(ij, dtype = 'uint16')
-        
-        if self.use_g4:
-            jik = [[j,0,k] for j in range(1, len(system))
-                   for k in range(1, len(system)) if k > j]
-            if len(jik) < 1:
-                raise RuntimeError(f'too few atoms within {self.r_max:.2f} A ',
-                    'of the absorption site to set up non-zero angular ',
-                    'symmetry functions (no triples)')
-            else:
-                jik = np.array(jik, dtype = 'uint16')
-        
-        rij = system.get_distances(ij[:,0], ij[:,1])
-        wacsf = self._g1(rij)
+        ij = np.array([[0,j] for j in range(1, len(system))], dtype = 'uint16')
 
-        if self.use_g2:
-            if self.with_weighting:
-                zi = system.get_atomic_numbers()[ij[:,0]] 
-                zj = system.get_atomic_numbers()[ij[:,1]]
-            else:
-                zi = zj = 1.0
+        if self.n_g4:
+            jik = np.array([[j,0,k] for j in range(1, len(system))
+                for k in range(1, len(system)) if k > j], dtype = 'uint16')
+
+        rij = system.get_distances(ij[:,0], ij[:,1])
+        g1 = np.sum(cosine_cutoff(rij, self.r_max))
+
+        wacsf = g1
+
+        if self.n_g2:
+            #zi = system.get_atomic_numbers()[ij[:,0]] 
+            zj = system.get_atomic_numbers()[ij[:,1]]
             rij = system.get_distances(ij[:,0], ij[:,1])
-            g2 = self._g2(zi, zj, rij)
+            g2 = self.g2_transformer.transform(zj, rij)
             wacsf = np.append(wacsf, g2)
 
-        if self.use_g4:
-            if self.with_weighting:
-                zi = system.get_atomic_numbers()[jik[:,1]]
-                zj = system.get_atomic_numbers()[jik[:,0]]
-                zk = system.get_atomic_numbers()[jik[:,2]]
-            else:
-                zi = zj = zk = 1.0
+        if self.n_g4:
+            #zi = system.get_atomic_numbers()[jik[:,1]]
+            zj = system.get_atomic_numbers()[jik[:,0]]
+            zk = system.get_atomic_numbers()[jik[:,2]]
             rij = system.get_distances(jik[:,1], jik[:,0])
             rik = system.get_distances(jik[:,1], jik[:,2])
             rjk = system.get_distances(jik[:,0], jik[:,2])
-            ajik = system.get_angles(jik)
-            g4 = self._g4(zi, zj, zk, rij, rik, rjk, ajik)
+            ajik = np.radians(system.get_angles(jik))
+            g4 = self.g4_transformer.transform(zj, zk, rij, rik, rjk, ajik)
             wacsf = np.append(wacsf, g4)
 
         return wacsf
 
-    def _fc(self, rij):
+    def get_len(self) -> int:
+        
+        return 1 + self.n_g2 + self.n_g4
 
-        return (np.cos((np.pi * rij) / self.r_max) + 1.0) / 2.0
+class SymmetryFunctionTransformer(ABC):
+    """
+    An abstract base class for generating symmetry function vectors.
+    """
 
-    def _r(self, rij, a, b):
+    def __init__(
+        self,
+        n: int,
+        r_min: float,
+        r_max: float,
+        parameterisation: str
+    ):
+        """
+        Args:
+            n (int): The number of symmetry functions to use for encoding.
+            r_min (float, optional): The minimum radial cutoff distance (in A)
+                around the absorption site; should be 0.0.
+                Defaults to 0.0.
+            r_max (float, optional): The maximum radial cutoff distance (in A)
+                around the absorption site.
+                Defaults to 8.0.
+            parameterisation (str, optional): The strategy to use for symmetry
+                function parameterisation; choices are 'shifted' or 'centred'.
+                For details, see Marquetand et al.; J. Chem. Phys., 2018, 148,
+                241709 (DOI: 10.1063/1.5019667).
+                Defaults to 'shifted'.
+        """
 
-        return np.exp(-1.0 * a * np.square(rij[:,np.newaxis] - b))
+        self.n = n
+        self.r_min = r_min
+        self.r_max = r_max
+        self.parameterisation = parameterisation
 
-    def _a(self, ajik, a, b):
+        if self.parameterisation == 'shifted':
+            r_aux = np.linspace(self.r_min + 0.5, self.r_max - 0.5, self.n)
+            dr = np.diff(r_aux)[0]
+            self.h = np.array([1.0 / (2.0 * (dr**2)) for _ in r_aux])
+            self.m = np.array([i for i in r_aux])
+        elif self.parameterisation == 'centred':
+            r_aux = np.linspace(self.r_min + 1.0, self.r_max - 0.5, self.n)
+            self.h = np.array([1.0 / (2.0 * (i**2)) for i in r_aux])
+            self.m = np.array([0.0 for _ in r_aux])
+        else:
+            err_str = 'parameterisation options: \'shifted\' | \'centred\'; ' \
+                'for details, see DOI: 10.1063/1.5019667'
+            raise ValueError(err_str)
 
-        return np.power(1.0 + (a * np.cos(np.radians(ajik))[:,np.newaxis]), b)
+    @abstractmethod
+    def transform(self, *args) -> np.ndarray:
+        """
+        Encodes structural information (`*args`) using symmetry functions.
 
-    def _g1(self, rij):
+        Returns:
+            np.ndarray: A symmetry function vector.
+        """
 
-        g1 = np.sum(self._fc(rij))
+        pass
 
-        return g1
+class G2SymmetryFunctionTransformer(SymmetryFunctionTransformer):
+    """
+    A class for generating G2 symmetry function vectors.
+    """
 
-    def _g2(self, zi, zj, rij):
+    def __init__(
+        self,
+        n: int,
+        r_min: float = 0.0,
+        r_max: float = 8.0,
+        parameterisation: str = 'shifted'
+    ):
+        """
+        Args:
+            n (int): The number of G2 symmetry functions to use for encoding.
+            r_min (float, optional): The minimum radial cutoff distance (in A)
+                around the absorption site; should be 0.0.
+                Defaults to 0.0.
+            r_max (float, optional): The maximum radial cutoff distance (in A)
+                around the absorption site.
+                Defaults to 8.0.
+            parameterisation (str, optional): The strategy to use for symmetry
+                function parameterisation; choices are 'shifted' or 'centred'.
+                For details, see Marquetand et al.; J. Chem. Phys., 2018, 148,
+                241709 (DOI: 10.1063/1.5019667).
+                Defaults to 'shifted'.
+        """
+        
+        super().__init__(
+            n,
+            r_min = r_min,
+            r_max = r_max,
+            parameterisation = parameterisation
+        )
 
-        g2 = (np.sum((zj * self._fc(rij))[:,np.newaxis] 
-            * self._r(rij, self.g2_h, self.g2_m), axis = 0))
+    def transform(
+        self,
+        zj: np.ndarray,
+        rij: np.ndarray
+    ) -> np.ndarray:
+
+        g2 = np.full(self.n, np.nan)
+
+        cutoff_ij = cosine_cutoff(rij, self.r_max)
+
+        i = 0
+        for h, m in zip(self.h, self.m):
+            g2[i] = np.sum(zj * gaussian(rij, h, m) * cutoff_ij)
+            i += 1
 
         return g2
 
-    def _g4(self, zi, zj, zk, rij, rik, rjk, ajik):
+class G4SymmetryFunctionTransformer(SymmetryFunctionTransformer):
+    """
+    A class for generating G4 symmetry function vectors.
+    """
 
-        g4 = (np.sum((zj * zk * self._fc(rij) * self._fc(rik)
-            * self._fc(rjk))[:,np.newaxis]
-            * self._r(rij, self.g4_h, self.g4_m)
-            * self._r(rik, self.g4_h, self.g4_m)
-            * self._r(rjk, self.g4_h, self.g4_m)
-            * self._a(ajik, self.g4_l, self.g4_z), axis = 0)
-        ) * np.power(2.0, 1.0 - self.g4_z)
+    def __init__(
+        self,
+        n: int,
+        r_min: float = 0.0,
+        r_max: float = 8.0,
+        l: list = [1.0, -1.0],
+        z: list = [1.0],
+        parameterisation: str = 'centred'
+    ):
+        """
+        Args:
+            n (int): The number of G4 symmetry functions to use for encoding.
+            r_min (float, optional): The minimum radial cutoff distance (in A)
+                around the absorption site; should be 0.0.
+                Defaults to 0.0.
+            r_max (float, optional): The maximum radial cutoff distance (in A)
+                around the absorption site.
+                Defaults to 8.0.
+            l (list, optional): List of lambda values for G4 symmetry function
+                encoding. For details, see Marquetand et al.; J. Chem. Phys.,
+                2018, 148, 241709 (DOI: 10.1063/1.5019667).
+                Defaults to [1.0, -1.0].
+            z (list, optional): List of zeta values for G4 symmetry function
+                encoding. For details, see Marquetand et al.; J. Chem. Phys.,
+                2018, 148, 241709 (DOI: 10.1063/1.5019667).
+                Defaults to [1.0].
+            parameterisation (str, optional): The strategy to use for symmetry
+                function parameterisation; choices are 'shifted' or 'centred'.
+                For details, see Marquetand et al.; J. Chem. Phys., 2018, 148,
+                241709 (DOI: 10.1063/1.5019667).
+                Defaults to 'centred'.
+        """
+        
+        super().__init__(
+            n,
+            r_min = r_min,
+            r_max = r_max,
+            parameterisation = parameterisation
+        )
+
+        if self.n % (len(l) * len(z)):
+            err_str = f'can\'t generate {self.n} G4 symmetry functions with ' \
+                f'{len(l)} lambda and {len(z)} zeta value(s)'
+            raise ValueError(err_str)
+        else:
+            n_ = int(self.n / (len(l) * len(z)))
+            self.h = self.h[:n_]
+            self.m = self.m[:n_]
+            self.l = np.array(l)
+            self.z = np.array(z)
+
+    def transform(
+        self,
+        zj: np.ndarray,
+        zk: np.ndarray,
+        rij: np.ndarray,
+        rik: np.ndarray,
+        rjk: np.ndarray,
+        ajik: np.ndarray
+    ) -> np.ndarray:
+
+        g4 = np.full(self.n, np.nan)
+
+        cutoff_ij = cosine_cutoff(rij, self.r_max)
+        cutoff_ik = cosine_cutoff(rik, self.r_max)
+        cutoff_jk = cosine_cutoff(rjk, self.r_max)
+
+        i = 0
+        for h, m in zip(self.h, self.m):
+            for l in self.l:
+                for z in self.z:
+                    g4[i] = np.sum(zj * zk * (1.0 + (l * np.cos(ajik)))**z
+                        * gaussian(rij, h, m) * cutoff_ij
+                        * gaussian(rik, h, m) * cutoff_ik
+                        * gaussian(rjk, h, m) * cutoff_jk
+                    ) * (2.0**(1.0 - z))
+                    i += 1
 
         return g4
 
-    def get_len(self) -> int:
+def cosine_cutoff(r: np.ndarray, r_max: float) -> np.ndarray:
+    # returns a cosine cutoff function defined over `r` with `r_max` defining
+    # the cutoff radius; see Behler; J. Chem. Phys., 2011, 134, 074106
+    # (DOI: 10.1063/1.3553717) 
 
-        return 1 + self.n_g2 + self.n_g4
+    return (np.cos((np.pi * r) / r_max) + 1.0) / 2.0
+
+def gaussian(r: np.ndarray, h: float, m: float) -> np.ndarray:
+    # returns a gaussian-like function defined over `r` with eta (`h`) defining
+    # the width and mu (`h`) defining the centrepoint/peak position
+
+    return np.exp(-1.0 * h * (r - m)**2)
