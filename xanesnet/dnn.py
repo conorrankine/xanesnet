3aa724e9aa394db19e13115698d0c7f651aac88e
Conor Rankine <conor.rankine@ncl.ac.uk>
2022-05-19 15:23:33 +0100

    added option to use dropout layers in dropout-active mode at prediction time with the `dropout_always_on` keyword

--- xanesnet/dnn.py
+++ xanesnet/dnn.py
@@ -30,6 +30,86 @@ from tensorflow.keras.layers import Activation
 from tensorflow.keras.callbacks import CSVLogger
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.utils import register_keras_serializable
+
+###############################################################################
+################################### CLASSES ###################################
+###############################################################################
+
+@register_keras_serializable()
+class Dropout_(Dropout):
+    """
+    A Dropout layer subclass of the default Keras Dropout layer that exposes
+    the `training` argument of the `call()` function to the Keras
+    `model.Sequential()` API; the `training` argument takes either 1 (True) or
+    0 (False) to indicate to the layer whether to operate in training
+    (i.e. dropout-active) or testing (i.e. dropout-inactive) mode, otherwise
+    the operating mode is automatically determined by Keras if the `training`
+    argument is left to default to None. 
+
+    If `always_on` is True, dropout will be active during training and
+    prediction (useful, e.g., in Monte-Carlo experiments); if `always_on` is
+    False, Keras will determine dropout activity automatically.
+
+    The default Keras Dropout layer randomly sets input units to zero with a
+    frequency of `rate` at each step during training time to help limit the
+    propensity for overfitting; inputs that are not set to zero are scaled up
+    by 1(1 - `rate`) such that the sum over all inputs is not changed.
+    """
+
+    def __init__(
+        self,
+        rate: float,
+        always_on: bool = False,
+        noise_shape: int = None,
+        seed: int = None,
+        **kwargs
+    ):
+        """
+        Args:
+            rate (float): Fraction (0.0 -> 1.0) of the input units to drop.
+            always_on (bool): Toggles whether the Dropout layer is always
+                called in dropout-active mode; `training` is passed to
+                `call()` with a value of 1 (True) if True, else None (and
+                automatically determined by Keras as usual) if False.
+                Defaults to False.
+            noise_shape (int): 1D integer tensor representing the shape of
+                the binary dropout mask to be multiplied with the input.
+                Defaults to None.
+            seed (int): An integer to use as a random seed.
+                Defaults to None.
+        """
+
+        super().__init__(
+            rate,
+            noise_shape = noise_shape,
+            seed = seed,
+            **kwargs
+        )
+
+        self.always_on = always_on
+
+    def call(self, inputs, training: bool = None):
+        """
+        Args:
+            inputs: Input tensor of any rank.
+            training: Toggles whether the Dropout layer is called in training
+                (dropout-active) or testing (dropout-inactive) mode.
+                Defaults to None.
+        """
+
+        output = super().call(
+            inputs, training = 1 if self.always_on else training
+        )
+
+        return output
+
+    def get_config(self):
+
+        config = super().get_config()
+        config.update({'always_on': self.always_on})
+
+        return config
 
 ###############################################################################
 ################################## FUNCTIONS ##################################
@@ -79,6 +159,7 @@ def build_mlp(
     loss: str = 'mse',
     lr: float = 0.001,
     dropout: float = 0.2,
+    dropout_always_on: bool = False,
     kernel_init: str = 'he_uniform',
     bias_init: str = 'zeros',
     random_state: RandomState = None
@@ -103,13 +184,13 @@ def build_mlp(
 
     net.add(Dense(hl_ini_dim, **ini_condition))
     net.add(Activation(activation))
-    net.add(Dropout(dropout))
+    net.add(Dropout_(dropout, always_on = dropout_always_on))
     
     for i in range(n_hl - 1):
         hl_dim = (int(hl_ini_dim * (hl_shrink ** (i + 1))))
         net.add(Dense(hl_dim if (hl_dim > 1) else 1, **ini_condition))
         net.add(Activation(activation))
-        net.add(Dropout(dropout))
+        net.add(Dropout_(dropout, always_on = dropout_always_on))
     
     net.add(Dense(out_dim))
     net.add(Activation('linear'))
