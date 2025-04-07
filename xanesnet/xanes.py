b3f7656013148b9eaa8d9e4b5cd0c942e9a71479
Conor Rankine <conor.rankine@ncl.ac.uk>
2025-04-07 12:21:59 +0100

    added `typing_extensions(>=4.13.1)` to `pyproject.toml` as a dependency; sourced `Self` from `typing_extensions` in `xanes.py`

--- xanesnet/xanes.py
+++ xanesnet/xanes.py
@@ -22,7 +22,8 @@ this program.  If not, see <https://www.gnu.org/licenses/>.
 import numpy as np
 import copy
 from pathlib import Path
-from typing import Union, TextIO, Self
+from typing import Union, TextIO
+from typing_extensions import Self
 from .templates import BaseTransformer
 
 ###############################################################################
